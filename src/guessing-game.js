5a0a35e12effbc57029969c5013094196ce2e1e5
Alexey Akulich <loki1987by@yandex.ru>
2021-07-02 19:29:05 +0300

    feat: set methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.numbers = []
+        this.version = null
+    }
 
     setRange(min, max) {
-
+        this.numbers = []
+        for(let i=min; i <= max; i++) {
+            this.numbers.push(i)
+        }
+        this.version = Math.ceil((min + max) / 2)
     }
 
     guess() {
-
+        return this.version;
     }
 
     lower() {
-
+        const min = this.numbers[0]
+        const max = this.version;
+        this.setRange(min, max)
     }
 
     greater() {
-
+        const min = this.version;
+        const last = this.numbers.length - 1 
+        const max = this.numbers[last]
+        this.setRange(min, max)
     }
 }
 
